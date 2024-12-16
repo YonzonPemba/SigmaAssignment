99b2af8e964bb80db5b0e8cd7ff9f8453b9c11a0
Pemba Tamang <I34928@verisk.com>
2024-12-16 11:31:35 +0545

    add repository to communicate with direct data source

---
+++ Repositories/Implementations/InMemoryCandidateRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigmaAssignment.Data.Entities;
+using SigmaAssignment.Repositories.Interfaces;
+
+namespace SigmaAssignment.Repositories.Implementations
+{
+    public class InMemoryCandidateRepository : ICandidateRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public InMemoryCandidateRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> GetByEmailAsync(string email)
+        {
+            return await _context.Candidates.FirstOrDefaultAsync(c => c.Email == email);
+        }
+
+        public async Task AddAsync(Candidate candidate)
+        {
+            await _context.Candidates.AddAsync(candidate);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Candidate candidate)
+        {
+            _context.Candidates.Update(candidate);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigmaAssignment.Data.Entities;
+
+namespace SigmaAssignment.Repositories.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> GetByEmailAsync(string email);
+        Task AddAsync(Candidate candidate);
+        Task UpdateAsync(Candidate candidate);
+    }
+}
